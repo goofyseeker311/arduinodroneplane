06b0bf7f95b416e2eaaf690f7bd859b983e43576
goofyseeker311 <goofyseeker311@gmail.com>
2025-07-13 03:47:34 +0300

    Update arduinodroneplane.ino
    added arduino wifi access point web server test code.

--- arduinodroneplane.ino
+++ arduinodroneplane.ino
@@ -1,6 +1,95 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+char ssid[] = "droneplane";
+char pass[] = "changeit";
+
+int led =  LED_BUILTIN;
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
 void setup() {
+  Serial.begin(9600);
+  while (!Serial) {}
+  Serial.println("Access Point Web Server");
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
+  }
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+  WiFi.config(IPAddress(10, 0, 0, 1));
+  Serial.print("Creating access point named: ");
+  Serial.println(ssid);
+  status = WiFi.beginAP(ssid, pass);
+  if (status != WL_AP_LISTENING) {
+    Serial.println("Creating access point failed");
+    while (true);
+  }
+  delay(10000);
+  server.begin();
+  printWiFiStatus();
+
   pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 void loop() {
+  if (status != WiFi.status()) {
+    status = WiFi.status();
+    if (status == WL_AP_CONNECTED) {
+      Serial.println("Device connected to AP");
+    } else {
+      Serial.println("Device disconnected from AP");
+    }
+  }
+  WiFiClient client = server.available();   // listen for incoming clients
+  if (client) {                             // if you get a client,
+    Serial.println("new client");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+            client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+            client.println();
+            break;
+          } else {      // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {    // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(led, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(led, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    client.stop();
+    Serial.println("client disconnected");
+  }
+
   analogWrite(3, 255);
+  analogWrite(5, 255);
+}
+void printWiFiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
